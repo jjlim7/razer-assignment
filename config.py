1ef8a4c223afdfec80debfad688f4e0cd38c32c4
jjlim7 <jj.limmm159@gmail.com>
2024-10-06 17:13:16 +0800

    Update config

--- config.py
+++ config.py
@@ -8,4 +8,8 @@ load_dotenv()
 class Config:
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
     CSV_FILE_PATH = os.environ.get("CSV_FILE_PATH")
-    GAME_DATA = load_csv_data(CSV_FILE_PATH)
+
+    if CSV_FILE_PATH:
+        GAME_DATA = load_csv_data(CSV_FILE_PATH)
+    else:
+        GAME_DATA = load_csv_data("data/games_description.csv")
